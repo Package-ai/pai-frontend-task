4b8126a050f1a0a479510b46d5df5155eb597286
Maor Barel <maor@package.ai>
2025-01-29 09:12:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-zinc-500 w-screen h-screen p-5">
+    <div
+      class="bg-white border border-gray-300 rounded-md p-3 max-w-[1200px] m-auto h-full !font-sans flex flex-col gap-y-2 overflow-hidden"
+    >
+      <ChatHeader class="flex-shrink-0" />
+      <div class="flex-grow overflow-y-auto bg-stone-100 rounded p-2 space-y-2">
+        <ChatMessage v-for="item in items" :key="item.id" :item="item" />
+      </div>
+    </div>
+    <ChatContainer />
+  </div>
+</template>
+
+<script setup>
+import ChatHeader from './components/ChatHeader.vue'
+import ChatMessage from './components/ChatMessage.vue'
+import items from './assets/msgs.json'
+</script>
---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex gap-x-2" :class="{ 'flex-row-reverse': !item.incoming }">
+    <div
+      class="w-9 h-9 rounded-full flex items-center justify-center text-lg text-white flex-shrink-0"
+      :class="{
+        'bg-green-600': item.incoming,
+        'bg-blue-600': !item.incoming,
+      }"
+      v-text="item.initials"
+    />
+    <div
+      :class="[
+        {
+          'rounded-e-xl rounded-es-xl bg-neutral-300': item.incoming,
+          'rounded-s-xl rounded-ee-xl bg-indigo-100': !item.incoming,
+        },
+      ]"
+      class="!shadow-none relative flex flex-col leading-1.5 p-4 border-gray-200 space-y-1 shadow-sm shadow-gray-300"
+    >
+      <div
+        class="flex items-center justify-between -mt-3"
+        :class="{ 'flex-row-reverse': !item.incoming }"
+      >
+        <span
+          :class="{ 'pr-4': item.incoming, 'pl-4': !item.incoming }"
+          class="text-sm font-bold text-text"
+          v-text="item.from"
+        />
+      </div>
+      <p
+        class="BaseMessage-content text-sm font-normal whitespace-break-spaces text-text break-words !break-all"
+        v-text="item.text"
+      />
+
+      <div
+        :class="{ 'justify-end': item.incoming }"
+        class="flex items-center space-x-1 !-mb-3 pt-2"
+      >
+        <span
+          class="text-xs font-normal text-gray-500"
+          :class="{ 'text-end': item.incoming }"
+          v-text="item.created"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ChatMessage',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
